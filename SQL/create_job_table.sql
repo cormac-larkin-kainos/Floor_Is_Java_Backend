cb25edae65d814a6a104210b505b691c34ef98ee
Cormac Larkin <cormac.larkin@kainos.com>
2023-11-07 14:34:20 +0000

    Created 'job' table with sample data and GET /api/jobs endpoint

--- SQL/create_job_table.sql
+++ SQL/create_job_table.sql
@@ -2,3 +2,13 @@ CREATE TABLE job (
     job_id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     job_title VARCHAR(255)
 );
+
+INSERT INTO job (job_title)
+        VALUES ('Senior Software Engineer'),
+        ('Dynamics Lead Engineer'),
+        ('Lead Software Engineer'),
+        ('Test Engineer'),
+        ('Innovation Lead'),
+        ('Technology Leader'),
+        ('Technical Architect'),
+        ('Software Engineer');
