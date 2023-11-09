e0e3d422cb616984ac4f462db0176022a34fe252
Ciara Ni Shiochru <ciarasugrue@gmail.com>
2023-11-09 09:59:57 +0000

    renaming files

--- src/main/java/org/kainos/ea/FloorIsJavaConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.kainos.ea;
-
-import io.dropwizard.Configuration;
-
-/**
- * Class to configure FloorIsJava application.
- */
-public final class FloorIsJavaConfiguration extends Configuration {
-}
