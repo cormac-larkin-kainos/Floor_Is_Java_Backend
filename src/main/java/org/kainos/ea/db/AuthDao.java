6c119966c692185815f861ad5cb8c67ac6730f05
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-11-20 10:49:23 +0000

    Linting violation

--- src/main/java/org/kainos/ea/db/AuthDao.java
+++ src/main/java/org/kainos/ea/db/AuthDao.java
@@ -9,22 +9,6 @@ public class AuthDao implements IAuthSource {
 
     private final DatabaseConnector databaseConnector = new DatabaseConnector();
 
-    public boolean validLogin(Login login) throws SQLException {
-
-        Connection conn = databaseConnector.getConnection();
-        String sqlQuery = "SELECT password FROM `user` WHERE username = ?";
-        PreparedStatement st = conn.prepareStatement(sqlQuery);
-
-        st.setString(1, login.getUsername());
-
-        ResultSet rs = st.executeQuery();
-
-        if (rs.next()) {
-            return rs.getString("password").equals(login.getPassword());
-        }
-        return false;
-    }
-
     @Override
     public HashedPassword getPasswordForUser(String username) throws AuthenticationException {
         String SQL = "SELECT password_hash, password_salt, password_hash_iterations FROM user WHERE email = ?";
@@ -34,12 +18,8 @@ public class AuthDao implements IAuthSource {
             st.setString(1,username);
 
             ResultSet rs = st.executeQuery();
-            if (rs.next()) {
-               return new HashedPassword(
-                   rs.getBytes("password_hash"),
-                   rs.getInt("password_hash_iterations"),
-                   rs.getBytes("password_salt")
-               );
+            if(rs.next()) {
+                return new HashedPassword(rs.getBytes("password_hash"), rs.getInt("password_hash_iterations"), rs.getBytes("password_salt"));
             }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
