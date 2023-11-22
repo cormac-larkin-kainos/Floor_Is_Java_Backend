f0b64c4c408802a4c2931993bf843ba0b20f5fda
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-11-22 09:31:32 +0000

    Integration fixes

--- src/test/java/org/kainos/ea/integration/JobControllerIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/JobControllerIntegrationTest.java
@@ -8,7 +8,9 @@ import org.kainos.ea.FloorIsJavaConfiguration;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.cli.Login;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -20,10 +22,24 @@ public class JobControllerIntegrationTest {
             new ResourceConfigurationSourceProvider()
     );
 
+    private static final String VALID_USERNAME = System.getenv("TEST_USERNAME");
+    private static final String VALID_PASSWORD = System.getenv("TEST_PASSWORD");
+
+    private String getJWT() {
+        if(VALID_USERNAME == null || VALID_PASSWORD == null){
+            throw new IllegalArgumentException("Test credential environment variables not set!");
+        }
+        Login credentials = new Login(VALID_USERNAME,VALID_PASSWORD);
+        Response response = APP.client().target("http://localhost:8080/api/login").request().post(Entity.json(credentials));
+
+        return response.readEntity(String.class);
+    }
+
     @Test
     void getAllJobs_ShouldReturnListOfJobs() {
         Response response = APP.client().target("http://localhost:8080/api/jobs")
                 .request()
+                .header("Authorization","Bearer " + getJWT())
                 .get();
 
         Assertions.assertEquals(200, response.getStatus());
